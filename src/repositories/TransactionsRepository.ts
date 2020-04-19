1ff6be7e4325054386ba3e0d87d67f0af00f2d24
VladeMelo <vlademirjos@hotmail.com>
2020-04-19 19:55:29 -0300

    Single Responsibility Principle

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,51 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
+
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
+  private total: number
 
   constructor() {
     this.transactions = [];
+    this.total = 0
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    let income = 0, outcome = 0
+    this.transactions.map(transaction => {
+      (transaction.type === 'income') ? income += transaction.value : outcome += transaction.value
+    })
+
+    const total = income - outcome
+    const balance:Balance = {income, outcome, total}
+
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title, value, type}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({title, type, value})
+    this.transactions.push(transaction)
+    if (type === 'income') {
+      this.total+=value
+    } else {
+      this.total-=value
+    }
+    return transaction
+  }
+
+  public getTotal(): number {
+    return this.total
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Execute {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,16 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Execute): Transaction {
+    if (type === 'outcome' && value > this.transactionsRepository.getTotal()) {
+      throw Error('Transaction not accepted, please check again!');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+    return transaction;
   }
 }
 
